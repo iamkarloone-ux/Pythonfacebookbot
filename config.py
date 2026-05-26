aca07c7d897e71d6a027e77467088edfaba48128
iamkarloone-ux <iamkarloone@gmail.com>
2026-05-26 23:03:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.wbilwngiibubigochqnc:GpU3eKg1yecj3LVq@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.wbilwngiibubigochqnc:Gpu3eKg1yecj3LVq@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres")
 PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "EAA5a1...")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "pinkGuineaFowl_onpella_2025")
 ADMIN_ID = os.getenv("ADMIN_ID", "9022271741140377")
