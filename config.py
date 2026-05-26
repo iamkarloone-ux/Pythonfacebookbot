f3e85a50806d1750f5624fb9cbc5e4f6d51d67ee
iamkarloone-ux <iamkarloone@gmail.com>
2026-05-26 18:42:08 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.vburnklpeuvxswtrugbp:abalunan1@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres")
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "EAA5a1...")
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "pinkGuineaFowl_onpella_2025")
+ADMIN_ID = os.getenv("ADMIN_ID", "9022271741140377")
+WORKER_SECRET_TOKEN = os.getenv("WORKER_SECRET_TOKEN", "CREATE_A_LONG_AND_SECRET_WORKER_PASSWORD_HERE")
