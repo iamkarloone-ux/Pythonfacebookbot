c56a2528a0538952dd8426ec126efabc7a98d94a
iamkarloone-ux <iamkarloone@gmail.com>
2026-05-27 10:38:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.rznrrywtfiyehwkfntfj:karloworksabalunan@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres")
-PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "EAA5a1El5oeIBPcqXg2AY2y0reyZAOnRj2GIveLqop4ibLMoTQZBNo7w2fkLaO6ZCvssgfhOsndEPiyZCv59SguqWt7bARCBtLBrZBB9Mbjkem5ZAtK06u9qTNv8Mj6z1fhZBMN3O7SWBwZACSEJjVTNCYxiat0tp7HChOPvrIhagCwX0j4faZBiZA3zthfrGd0C7iMRTMvQRoe1GAuSJJIWTwRkn9FdwZDZD")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "pinkGuineaFowl_onpella_2025")
 ADMIN_ID = os.getenv("ADMIN_ID", "9022271741140377")
-WORKER_SECRET_TOKEN = os.getenv("WORKER_SECRET_TOKEN", "CREATE_A_LONG_AND_SECRET_WORKER_PASSWORD_HERE")
-OPENROUTER_API_KEY = os.getenv("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")
-PYTHONUNBUFFERED= os.getenv("1")
+WORKER_SECRET_TOKEN = os.getenv("WORKER_SECRET_TOKEN", "")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
+PYTHONUNBUFFERED = os.getenv("PYTHONUNBUFFERED", "1")
